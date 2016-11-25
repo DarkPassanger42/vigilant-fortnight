cc18cb7072c4c349e386b0c9f2808bd2d454b336
DarkPassager42 <andrius.andrijauskas@iCloud.com>
2016-11-25 15:38:37 -0500

    added placeholder confirmation page, there is an issue with session management for different users - right not there is no way to differentiate between different users... need to work this out

--- src/main/java/jhu/edu/WebStore/ViewsAndControl/ConfirmationView.java
+++ src/main/java/jhu/edu/WebStore/ViewsAndControl/ConfirmationView.java
@@ -1,11 +1,13 @@
 package jhu.edu.WebStore.ViewsAndControl;
 
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import jhu.edu.WebStore.Data.Product;
+import jhu.edu.WebStore.Data.ShoppingCart;
 import jhu.edu.WebStore.WebStoreUI;
 
-/**
- * Created by andri on 11/25/2016.
- */
+
 public class ConfirmationView extends BaseView {
 
     WebStoreUI parentUI;
@@ -21,8 +23,28 @@ public class ConfirmationView extends BaseView {
         super.enter(event);
         RefreshLogInOutComponent();
 
+        //this needs to still be done...
+        //not sure what approach to take
 
 
+        //get all products in the cart from the session
+        ShoppingCart cart = (ShoppingCart) parentUI.getSession().getAttribute("ShoppingCart");
+
+        HorizontalLayout checkOutLayout = new HorizontalLayout();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Product product : cart.getItems()) {
+            sb.append(product.getName());
+            sb.append(product.getDescription());
+            sb.append("\n");
+        }
+
+        Label label = new Label(sb.toString());
+
+        checkOutLayout.addComponent(label);
+
+        addComponent(checkOutLayout);
     }
 
 }
